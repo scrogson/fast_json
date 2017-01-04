99d75a7061372c62042371b1806da978449a9d1c
Jason Orendorff <jason.orendorff@gmail.com>
2017-01-04 10:26:23 -0600

    This weird trait-method call syntax isn't necessary anymore.

--- native/fast_json/src/decoder.rs
+++ native/fast_json/src/decoder.rs
@@ -1,4 +1,4 @@
-use rustler::{NifDecoder, NifEncoder, NifEnv, NifTerm, NifResult};
+use rustler::{NifEncoder, NifEnv, NifTerm, NifResult};
 use rustler::tuple::make_tuple;
 use rustler::atom::get_atom;
 use ::errors::*;
@@ -14,7 +14,7 @@ use ::parser;
 // decode the resource and make a parser from it
 // decode sink stack and make a new TermSink::new(env, stack)
 pub fn naive_parse<'a>(env: &'a NifEnv, args: &Vec<NifTerm>) -> NifResult<NifTerm<'a>> {
-    let source = <String as NifDecoder>::decode(args[0])?;
+    let source = args[0].decode()?;
 
     match parser::naive(env, source) {
         Ok(term) => ok(env, term),
